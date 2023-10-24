71d23bbf58e00922e394d8149f8d8f37fd6773a2
Nadejda Barbarov <nadejda.barbarov@isa.utm.com>
2023-10-24 17:16:52 +0300

    Display files in console

--- src/FileLister.java
+++ src/FileLister.java
@@ -5,7 +5,13 @@ public class FileLister {
     public static void registerFile(String fileName, FileWriter fileWriter) {
         try {
             String fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
-            fileWriter.write("File: " + fileName + ", Extension: " + fileExtension + "\n");
+
+            // Write the full information to the text file
+            String fileData = "File: " + fileName + ", Extension: " + fileExtension;
+            fileWriter.write(fileData + "\n");
+
+            // Display only the file name in the console
+            System.out.println(fileName);
         } catch (IOException e) {
             System.out.println("An error occurred while writing to the text file.");
             e.printStackTrace();
