e92be500da6607fc262fd42f11e3912e614bcf58
Nadejda Barbarov <nadejda.barbarov@isa.utm.com>
2023-10-24 15:55:00 +0300

    Parent class created

--- src/FileLister.java
+++ src/FileLister.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class FileLister {
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileLister {
+    public static void registerFile(String fileName, FileWriter fileWriter) {
+        try {
+            String fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
+            fileWriter.write("File: " + fileName + ", Extension: " + fileExtension + "\n");
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the text file.");
+            e.printStackTrace();
+        }
+    }
 }
--- src/ListFilesInFolder.java
+++ src/ListFilesInFolder.java
@@ -1,62 +1,39 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.nio.file.FileVisitOption;
-import java.nio.file.FileVisitResult;
-import java.nio.file.FileVisitOption;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.attribute.FileTime;
-import java.util.EnumSet;
+import java.io.*;
+import java.util.Arrays;
 
 class ListFilesInFolder {
     public static void main(String[] args) {
         // Specify the folder path
-        String folderPath = "D:\\Semestrul 3\\OOP";
+        String folderPath = "D:\\exemplu";
 
-        // File object for the folder
-        File folder = new File(folderPath);
+        // Specify the output file path
+        String outputFilePath = "file_list.txt";
 
-        // Check if the folder exists
-        if (folder.exists() && folder.isDirectory()) {
-            // List all files and subdirectories
+        // Create a FileWriter for the output file
+        try {
+            FileWriter fileWriter = new FileWriter(outputFilePath);
+            File folder = new File(folderPath);
+            iterateAndRegisterFiles(folder, fileWriter);
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while creating the output file.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void iterateAndRegisterFiles(File folder, FileWriter fileWriter) {
+        if (folder.isDirectory()) {
             File[] files = folder.listFiles();
-
             if (files != null) {
-                try {
-                    // Create a FileWriter and PrintWriter to write to a text file
-                    FileWriter fileWriter = new FileWriter("file_list.txt");
-                    PrintWriter printWriter = new PrintWriter(fileWriter);
-
-                    for (File file : files) {
-                        if (file.isFile()) {
-                            String fileName = file.getName();
-                            String fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
-
-                            // Get file creation time
-                            Path filePath = file.toPath();
-                            BasicFileAttributes fileAttributes = Files.readAttributes(filePath, BasicFileAttributes.class);
-                            FileTime creationTime = fileAttributes.creationTime();
-
-                            printWriter.println("File: " + fileName + ", Extension: " + fileExtension + ", Created: " + creationTime);
-                            System.out.println("File: " + fileName);
-                        } else if (file.isDirectory()) {
-                            System.out.println("Directory: " + file.getName());
-                        }
+                for (File file : files) {
+                    if (file.isFile()) {
+                        String fileName = file.getName();
+                        FileLister.registerFile(fileName, fileWriter);
+                    } else if (file.isDirectory()) {
+                        iterateAndRegisterFiles(file, fileWriter);
                     }
-
-                    // Close the PrintWriter and FileWriter
-                    printWriter.close();
-                    fileWriter.close();
-                } catch (IOException e) {
-                    System.out.println("An error occurred while writing to the text file.");
-                    e.printStackTrace();
                 }
             }
-        } else {
-            System.out.println("The specified folder does not exist or is not a directory.");
         }
     }
 }
