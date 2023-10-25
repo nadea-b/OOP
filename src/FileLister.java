a46f0e15ef97092ede17da98864b51edf5d22b9b
Nadejda Barbarov <nadejda.barbarov@isa.utm.com>
2023-10-25 13:37:11 +0300

    Program iterates and records data about files

--- src/FileLister.java
+++ src/FileLister.java
@@ -1,20 +1,50 @@
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
 
 public class FileLister {
-    public static void registerFile(String fileName, FileWriter fileWriter) {
-        try {
-            String fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
+    public static void main(String[] args) {
+        // Specify the folder path
+        String folderPath = "D:\\exemplu";
 
-            // Write the full information to the text file
-            String fileData = "File: " + fileName + ", Extension: " + fileExtension;
-            fileWriter.write(fileData + "\n");
+        File folder = new File(folderPath);
+        iterateAndProcessFiles(folderPath, folder);
+    }
 
-            // Display only the file name in the console
-            System.out.println(fileName);
-        } catch (IOException e) {
-            System.out.println("An error occurred while writing to the text file.");
-            e.printStackTrace();
+    public static void iterateAndProcessFiles(String folderPath, File folder) {
+        if (folder.isDirectory()) {
+            File[] files = folder.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isFile()) {
+                        String fileName = file.getName();
+                        String fileExtension = getFileExtension(fileName);
+
+                        // Manually call the processing class based on the file extension
+                        if ("txt".equalsIgnoreCase(fileExtension)) {
+                            TextFileProcessor textFileProcessor = new TextFileProcessor();
+                            textFileProcessor.processTextFiles(folderPath, "file_list.txt");
+                        } else if ("jpg".equalsIgnoreCase(fileExtension) || "png".equalsIgnoreCase(fileExtension)) {
+                            ImageFileProcessor imageFileProcessor = new ImageFileProcessor();
+                            imageFileProcessor.processImageFiles(folderPath, "file_list.txt");
+                        } else if ("py".equalsIgnoreCase(fileExtension)){
+                            PythonFileProcessor pythonFileProcessor = new PythonFileProcessor();
+                            pythonFileProcessor.processPythonFiles(folderPath, "file_list.txt");
+                        } else if ("java".equalsIgnoreCase(fileExtension)){
+                            JavaFileProcessor javaFileProcessor = new JavaFileProcessor();
+                            javaFileProcessor.processJavaFiles(folderPath, "file_list.txt");
+                        } else System.out.println("No processing class found for file: " + fileName);
+                    } else if (file.isDirectory()) {
+                        iterateAndProcessFiles(folderPath, file);
+                    }
+                }
+            }
         }
+    }
+
+    public static String getFileExtension(String fileName) {
+        int dotIndex = fileName.lastIndexOf('.');
+        if (dotIndex > 0 && dotIndex < fileName.length() - 1) {
+            return fileName.substring(dotIndex + 1);
+        }
+        return "";
     }
 }
