de990c16c88a65a26dfcffaf61845cd20e788e82
Nadejda Barbarov <nadejda.barbarov@isa.utm.com>
2023-10-25 15:18:02 +0300

    Register change date

--- src/FileProcessor.java
+++ src/FileProcessor.java
@@ -1,6 +1,10 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -18,6 +22,8 @@ public class FileProcessor {
                             String fileName = file.getName();
                             String fileExtension = getFileExtension(fileName);
                             String creationDate = getFileCreationDate(file);
+                            String lastModifiedTime = getFileLastModifiedTime(file);
+
 
                             // Create a string with the file information
                           //  String fileData = "File: " + fileName + ", Extension: " + fileExtension + ", Creation Date: " + creationDate;
@@ -43,14 +49,29 @@ public class FileProcessor {
     }
 
     protected String getFileCreationDate(File file) {
+        Path filePath = file.toPath();
         try {
-            long creationTime = file.lastModified();
+            BasicFileAttributes attrs = Files.readAttributes(filePath, BasicFileAttributes.class);
+            FileTime creationTime = attrs.creationTime();
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            Date date = new Date(creationTime);
-            return dateFormat.format(date);
+            return dateFormat.format(creationTime.toMillis());
         } catch (Exception e) {
             System.err.println("Error getting creation date: " + e.getMessage());
             return "N/A";
         }
     }
+
+    protected String getFileLastModifiedTime(File file) {
+        Path filePath = file.toPath();
+        try {
+            BasicFileAttributes attrs = Files.readAttributes(filePath, BasicFileAttributes.class);
+            FileTime lastModifiedTime = attrs.lastModifiedTime();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            return dateFormat.format(lastModifiedTime.toMillis());
+        } catch (Exception e) {
+            System.err.println("Error getting last modified time: " + e.getMessage());
+            return "N/A";
+        }
+    }
+
 }
