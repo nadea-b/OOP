ba2d8b532e1ea4af4679645498988fb1819d86d4
Nadejda Barbarov <nadejda.barbarov@isa.utm.com>
2023-10-26 14:38:04 +0300

    Run status every 5 seconds

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+        executorService.scheduleAtFixedRate(() -> {
+            System.out.println("Running 'Status' command in the background...");
+            DirectoryComparator.main(new String[0]);
+            FileLister.main(new String[0]);
+        }, 0, 5, TimeUnit.SECONDS);
 
         while (true) {
             System.out.println("\nMain Menu\nChoose one option:");
@@ -23,23 +35,17 @@ public class Main {
                     System.out.println("Snapshot taken at: " + snapshotRecorder.getSnapshotTime());
                     snapshotRecorder.saveStateToTextFile("snapshot.txt"); // Save the state to a text file
                     break;
-
-
                 case 2:
-
                     // Call the main method of FileInfoSearcher to search for the specified file in the file list
                     FileInfoSearcher.main(new String[0]);
-
                     break;
-
                 case 3:
                     System.out.println("You selected 'Status'");
-                    DirectoryComparator.main(new String[0]);
-                    FileLister.main(new String[0]);
                     break;
                 case 4:
                     System.out.println("Exiting the program. Goodbye!");
                     scanner.close();
+                    executorService.shutdown();
                     System.exit(0);
                 default:
                     System.out.println("Invalid choice. Please select a valid option (1-4).");
