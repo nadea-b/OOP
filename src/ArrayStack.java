18904e78b3972867ded0d08e6667a462256b0c10
Nadejda Barbarov <nadejda.barbarov@isa.utm.com>
2023-11-12 15:41:59 +0200

    Array Stack implementation

--- src/ArrayStack.java
+++ src/ArrayStack.java
@@ -1,39 +1,39 @@
-import java.util.Arrays;
+import java.util.EmptyStackException;
 
-public class ArrayStack<T> implements Stack<T> {
+public class ArrayStack<E> implements Stack<E> {
     private static final int DEFAULT_CAPACITY = 10;
-    private Object[] elements;
+    private E[] array;
     private int size;
 
     public ArrayStack() {
-        elements = new Object[DEFAULT_CAPACITY];
+        array = (E[]) new Object[DEFAULT_CAPACITY];
         size = 0;
     }
 
     @Override
-    public void push(T item) {
-        if (size == elements.length) {
+    public void push(E item) {
+        if (size == array.length) {
             resizeArray();
         }
-        elements[size++] = item;
+        array[size++] = item;
     }
 
     @Override
-    public T pop() {
+    public E pop() {
         if (isEmpty()) {
-            throw new IllegalStateException("Stack is empty");
+            throw new EmptyStackException();
         }
-        T item = peek();
-        elements[--size] = null; // Clear the reference to the popped item
+        E item = array[--size];
+        array[size] = null; // Avoid memory leak
         return item;
     }
 
     @Override
-    public T peek() {
+    public E peek() {
         if (isEmpty()) {
-            throw new IllegalStateException("Stack is empty");
+            throw new EmptyStackException();
         }
-        return (T) elements[size - 1];
+        return array[size - 1];
     }
 
     @Override
@@ -47,7 +47,23 @@ public class ArrayStack<T> implements Stack<T> {
     }
 
     private void resizeArray() {
-        int newCapacity = elements.length * 2;
-        elements = Arrays.copyOf(elements, newCapacity);
+        int newCapacity = array.length * 2;
+        E[] newArray = (E[]) new Object[newCapacity];
+        System.arraycopy(array, 0, newArray, 0, size);
+        array = newArray;
+    }
+
+    public static void main(String[] args) {
+        ArrayStack<Integer> stack = new ArrayStack<>();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        System.out.println("Top element: " + stack.peek()); // Should print 3
+
+        while (!stack.isEmpty()) {
+            System.out.println("Popped: " + stack.pop());
+        }
     }
 }
